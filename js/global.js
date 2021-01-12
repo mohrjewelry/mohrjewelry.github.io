83cbc0f43db2f93d6568ea78a28c08d6cd073cd6
mohrjewelry <66364001+mohrjewelry@users.noreply.github.com>
2021-01-12 22:44:55 +0530

    Update global.js
    Pushed the end date to 15th Feb

--- js/global.js
+++ js/global.js
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 $( function() {
 // Add background image
 	$.backstretch('images/road2.jpg');
-	var endDate = "October  4, 2020 18:00:00";
+	var endDate = "February  15, 2021 18:00:00";
 	$('.countdown.simple').countdown({ date: endDate });
 	$('.countdown.styled').countdown({
 	  date: endDate,
